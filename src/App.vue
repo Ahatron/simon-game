40b126503e6755f3d0464d34ad935acf9f4f1e0c
Ahatron <ahatpolatov@gmail.com>
2024-03-19 10:53:21 +0500

    first

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,24 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="game-device">
+      <button class="game-device__btn top-left-btn"
+        style=""></button>
+      <button class="game-device__btn top-right-btn"
+        style=""></button>
+
+      <button class="game-device__btn bottom-left-btn"
+        style=""></button>
+      <button class="game-device__btn bottom-right-btn"
+        style=""></button>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+
 }
 </script>
 
@@ -21,8 +27,83 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  box-sizing: border-box;
+}
+
+.game-device {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  align-content: space-between;
+  width: 20rem;
+  height: 20rem;
+  background-color: white;
+  border-radius: 50%;
+  padding: 1rem;
+  box-shadow: 0px 0px 10px black;
+}
+
+.game-device__btn {
+  width: 49%;
+  height: 49%;
+  padding: 0;
+  border: none;
+  transition: .2s;
+}
+
+.top-left-btn {
+  border-top-left-radius: 100%;
+  background-color: rgb(113, 181, 204);
+}
+
+.top-left-btn:hover {
+  background-color: rgb(0, 103, 138);
+}
+
+.top-left-btn:active {
+  background-color: rgb(0, 191, 255);
+}
+
+
+.top-right-btn {
+  border-top-right-radius: 100%;
+  background-color: rgb(218, 106, 106);
+}
+
+.top-right-btn:hover {
+  background-color: rgb(150, 59, 59);
+}
+
+.top-right-btn:active {
+  background-color: rgb(255, 0, 0);
+}
+
+.bottom-right-btn {
+  border-bottom-right-radius: 100%;
+  background-color: rgb(101, 218, 101);
+}
+
+.bottom-right-btn:hover {
+  background-color: rgb(33, 139, 33);
+}
+
+.bottom-right-btn:active {
+  background-color: rgb(41, 228, 41);
+}
+
+
+.bottom-left-btn {
+  border-bottom-left-radius: 100%;
+  background-color: rgb(224, 224, 112);
+}
+
+.bottom-left-btn:hover {
+  background-color: rgb(194, 194, 61);
+}
+
+.bottom-left-btn:active {
+  background-color: rgb(255, 255, 45);
 }
 </style>
